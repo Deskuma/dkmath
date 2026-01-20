f78d07c00e66123f039fdc1163e922dabb6939df
Deskuma <deskuma.software@gmail.com>
2026-01-20 10:56:03 +0900

    Basic.leanを新規作成し、RHに関する基本定義とユーティリティを追加

---
+++ lean/dk_math/DkMath/RH/Basic.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+namespace DkMath.RH.Basic
+
+instance : Inhabited String := ⟨"DkMath RH Basic"⟩
+
+def ident := "DkMath: RH: Basic"
+def name := "DkMath Riemann Hypothesis - RH: Basic Module"
+def description := "Basic Definitions and Utilities for RH"
+
+end DkMath.RH.Basic
