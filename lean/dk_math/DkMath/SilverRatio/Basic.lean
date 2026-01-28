a9a40e49e284bd045461d815437956d8223641e5
Deskuma <deskuma.software@gmail.com>
2026-01-28 10:13:05 +0900

    Add: initial implementation of Silver Ratio basic definitions
    This commit introduces the foundational structure for the Silver Ratio
    module, including necessary imports.
    
    - Created Basic.lean file
    - Added copyright and licensing information
    - Imported SilverRatioUnit and SilverRatioCircle modules

---
+++ lean/dk_math/DkMath/SilverRatio/Basic.lean
@@ -0,0 +1,8 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import DkMath.SilverRatio.SilverRatioUnit
+import DkMath.SilverRatio.SilverRatioCircle
