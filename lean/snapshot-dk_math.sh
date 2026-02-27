50a64cbca5288ee38703528538b2a7658417be0b
Deskuma <deskuma.software@gmail.com>
2026-02-27 16:52:39 +0900

    Add: Create snapshot script for dk_math directory
    This script compresses the dk_math directory into a tar.gz file
    while excluding specific directories and dotfiles.
    
    - Introduced snapshot-dk_math.sh for creating snapshots
    - Excluded .lake, .vscode, .github, .git, and notes directories
    - Added checksum generation for the created tar.gz file

---
+++ lean/snapshot-dk_math.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DTM=$(date +%y%m%d-%H%M)
+
+# dk_math ディレクトリを tar.gz 形式で圧縮する
+# .lake と .github .git ディレクトリを除外する
+# 各ディレクトリ内のドットファイルも除外する
+tar cvzf __snapshot-dk_math-lean-code-$DTM.tar.gz \
+    --exclude='./dk_math/.lake' \
+    --exclude='./dk_math/.vscode' \
+    --exclude='./dk_math/.github' \
+    --exclude='./dk_math/.git' \
+    --exclude='./dk_math/notes' \
+    --exclude='./**/.pytest_cache' \
+    --exclude='./**/__pycache__/' \
+    --exclude='./**/__*' \
+    ./dk_math
+sha256sum __snapshot-dk_math-lean-code-$DTM.tar.gz > __snapshot-dk_math-lean-code-$DTM.tar.gz.sha256
