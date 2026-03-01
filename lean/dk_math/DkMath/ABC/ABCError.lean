2e78127a9a8313656f83457add535a403105673c
Deskuma <deskuma.software@gmail.com>
2026-03-02 05:14:27 +0900

    Add: New files for ABC module implementation
    This commit introduces several new files
    to establish the foundational structure for
    the ABC module.
    
    - Added ABC.lean for module documentation
    - Added ABC099.lean with initial definitions and imports
    - Added ABCError.lean for error handling
    - Added Main.lean as the entry point for the ABC module

---
+++ lean/dk_math/DkMath/ABC/ABCError.lean
@@ -0,0 +1,31 @@
+/-
+Copyright (c) 2025 D. and Wise Wolf. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import DkMath.ABC.ABC090
+
+#print "file: DkMath.ABC.ABCError"
+
+set_option linter.style.longLine false
+set_option linter.style.emptyLine false
+
+/- Note:
+※細分化前にエラー／警告のファイル。
+  ここでエラーを取り除いてから ABCNoError.lean に移動する。
+-/
+
+namespace ABC
+
+open scoped BigOperators
+
+open Nat Real Rat Filter Finset
+open MeasureTheory ProbabilityTheory
+
+-- -------------------------------------------------------
+
+
+-- -------------------------------------------------------
+
+end ABC
