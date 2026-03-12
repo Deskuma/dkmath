4379294c7b453e6c36f08dd4342120e52dd9e705
Deskuma <deskuma.software@gmail.com>
2026-03-12 11:34:01 +0900

    Add: Create Basic.lean and README.md for DkMath library
    This commit introduces the Basic.lean and README.md files
    to establish the foundational structure for the DkMath library.
    
    - Add Basic.lean with copyright and namespace declaration
    - Create README.md outlining the purpose of DkMath.Lib
    - Update lakefile.toml to include DkMathlib library

---
+++ lean/dk_math/DkMath/Lib/Basic.lean
@@ -0,0 +1,19 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import Mathlib
+
+namespace DkMath.Lib
+
+section Public
+
+end Public
+
+section Preview
+
+end Preview
+
+end DkMath.Lib
---
+++ lean/dk_math/DkMathlib/Basic.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import Mathlib
+
+namespace DkMath.Basic
+
+end DkMath.Basic
