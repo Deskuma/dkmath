ccefe927edd8174a2684ee4eb3cc9f8ece0249f7
Deskuma <deskuma.software@gmail.com>
2026-03-20 15:16:48 +0900

    Update: Improve git-diff script for Markdown formatting
    Enhanced the script to correctly format the output for Markdown
    with appropriate backslashes.
    
    - Updated backslash usage in comments for clarity
    - Adjusted output formatting for __git.diff file

--- lean/dk_math/git-diff.sh
+++ lean/dk_math/git-diff.sh
@@ -3,9 +3,11 @@
 # This script generates a diff of the current Git repository and opens it in VS Code.
 # This is the diff from the current tracked state. New files and untracked changes are not included.
 # You can specify a commit hash to get a diff that includes the current changes.
-# Use four backslashes to avoid nested code blocks in Markdown.
+# Use four backslashes `diff` and five backslashes `md` to avoid nested code blocks in Markdown.
 
-echo '````diff' > __git.diff
+echo '`````md' > __git.diff
+echo '````diff' >> __git.diff
 git diff $@ >> __git.diff
 echo '````' >> __git.diff
+echo '`````' >> __git.diff
 code __git.diff
