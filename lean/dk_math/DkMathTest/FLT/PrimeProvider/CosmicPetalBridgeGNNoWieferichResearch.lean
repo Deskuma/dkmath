316355e57e1be29562a5af590017d4dc7fb16d93
Deskuma <deskuma.software@gmail.com>
2026-06-14 05:02:37 +0900

    Add: DkMathTest: DkMathTest.FLT.PrimeProvider.CosmicPetalBridgeGNNoWieferichResearch

---
+++ lean/dk_math/DkMathTest/FLT/PrimeProvider/CosmicPetalBridgeGNNoWieferichResearch.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import DkMath.FLT.PrimeProvider.CosmicPetalBridgeGNNoWieferichResearch
+
+#print "file: DkMathTest.FLT.PrimeProvider.CosmicPetalBridgeGNNoWieferichResearch"
+
+namespace DkMath.FLT
+
+-- OK: all no-sorry: 2026/06/14  4:56
+#print axioms DkMath.FLT.primitivePrimeFactorOfDiffPow_of_FLT_branch
+#print axioms DkMath.FLT.triominoPrimitivePrimeFactorPadicValNatLeOneTarget_of_noLiftGNBridge
+#print axioms DkMath.FLT.triominoPrimitivePrimeFactorPadicValNatLeOneTarget_of_squarefreeGNBridge
+
+end DkMath.FLT
