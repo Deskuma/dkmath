634dd1613296546a062deecd74e981df8c86065e
D. <Deskuma@users.noreply.github.com>
2026-07-19 06:24:18 +0900

    Add cp-001 axiom checks

--- lean/dk_math/DkMathTest/FLT/Five/CheckAxioms.lean
+++ lean/dk_math/DkMathTest/FLT/Five/CheckAxioms.lean
@@ -9,5 +9,8 @@ import DkMath.FLT.Five.Standalone
 
 #print axioms DkMath.FLT.Five.add_pow_five_eq_add_mul_GN5
 #print axioms DkMath.FLT.Five.GN5_one_one_not_fifth_power
+#print axioms DkMath.FLT.Five.CleanGN5Channel.not_sq_dvd_body
+#print axioms DkMath.FLT.Five.not_fifth_power_body_of_clean
 #print axioms DkMath.FLT.Five.body5_eq_fifth_power_of_fermat
+#print axioms DkMath.FLT.Five.counterexample_false_of_clean_GN5Channel_by_dvd
 #print axioms DkMath.FLT.Five.Standalone.add_pow_five_eq_add_mul_GN5
