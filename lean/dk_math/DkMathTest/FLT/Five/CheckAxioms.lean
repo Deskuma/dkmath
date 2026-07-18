81754ce2d392979b0b6504a78e1150201821b82a
D. <Deskuma@users.noreply.github.com>
2026-07-19 05:21:16 +0900

    Add FLT5 experiment axiom checks

---
+++ lean/dk_math/DkMathTest/FLT/Five/CheckAxioms.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2026 D. and Wise Wolf. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: D. and Wise Wolf.
+-/
+
+import DkMath.FLT.Five
+import DkMath.FLT.Five.Standalone
+
+#print axioms DkMath.FLT.Five.add_pow_five_eq_add_mul_GN5
+#print axioms DkMath.FLT.Five.GN5_one_one_not_fifth_power
+#print axioms DkMath.FLT.Five.body5_eq_fifth_power_of_fermat
+#print axioms DkMath.FLT.Five.Standalone.add_pow_five_eq_add_mul_GN5
