e8763ec73c71a5575a685181f4da0890ecedb8ec
D. <Deskuma@users.noreply.github.com>
2026-07-24 03:45:10 +0900

    Wire positive terminal weighted bridge

--- lean/dk_math/DkMath/FLT/Seven.lean
+++ lean/dk_math/DkMath/FLT/Seven.lean
@@ -40,6 +40,7 @@ import DkMath.FLT.Seven.SevenBaseFirstOrderLinearization
 import DkMath.FLT.Seven.SevenBaseUnitSectorClassification
 import DkMath.FLT.Seven.SevenBaseLoadQuotient
 import DkMath.FLT.Seven.SevenBaseTerminalPacket
+import DkMath.FLT.Seven.SevenBaseTerminalWeightedBridge
 import DkMath.FLT.Seven.SevenBaseTerminalAudit
 
 #print "file: DkMath.FLT.Seven"
