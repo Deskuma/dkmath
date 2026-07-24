54e3ac7bf1b186d834931e4999c69e62d4c65723
D. <Deskuma@users.noreply.github.com>
2026-07-24 11:08:30 +0900

    Expose fixed terminal routing packet

--- lean/dk_math/DkMath/FLT/Seven.lean
+++ lean/dk_math/DkMath/FLT/Seven.lean
@@ -44,6 +44,7 @@ import DkMath.FLT.Seven.SevenBaseTerminalWeightedBridge
 import DkMath.FLT.Seven.SevenBaseTerminalLoadDivisibility
 import DkMath.FLT.Seven.SevenBaseTerminalEndpointSeparation
 import DkMath.FLT.Seven.SevenBaseTerminalCarrierRouting
+import DkMath.FLT.Seven.SevenBaseTerminalFixedRouting
 import DkMath.FLT.Seven.SevenBaseTerminalAudit
 
 #print "file: DkMath.FLT.Seven"
