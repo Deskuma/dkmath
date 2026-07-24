afa8004ed963cfd79cc0a01186ff9ca58b966d93
D. <Deskuma@users.noreply.github.com>
2026-07-24 11:44:22 +0900

    Expose terminal prime address API

--- lean/dk_math/DkMath/FLT/Seven.lean
+++ lean/dk_math/DkMath/FLT/Seven.lean
@@ -45,6 +45,7 @@ import DkMath.FLT.Seven.SevenBaseTerminalLoadDivisibility
 import DkMath.FLT.Seven.SevenBaseTerminalEndpointSeparation
 import DkMath.FLT.Seven.SevenBaseTerminalCarrierRouting
 import DkMath.FLT.Seven.SevenBaseTerminalFixedRouting
+import DkMath.FLT.Seven.SevenBaseTerminalPrimeAddress
 import DkMath.FLT.Seven.SevenBaseTerminalAudit
 
 #print "file: DkMath.FLT.Seven"
