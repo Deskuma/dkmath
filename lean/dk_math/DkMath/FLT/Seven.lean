6099bdd4c36f46aeb5b521abe6143e3dea35bd3f
D. <Deskuma@users.noreply.github.com>
2026-07-24 09:39:46 +0900

    Wire terminal carrier routing into Seven facade

--- lean/dk_math/DkMath/FLT/Seven.lean
+++ lean/dk_math/DkMath/FLT/Seven.lean
@@ -43,6 +43,7 @@ import DkMath.FLT.Seven.SevenBaseTerminalPacket
 import DkMath.FLT.Seven.SevenBaseTerminalWeightedBridge
 import DkMath.FLT.Seven.SevenBaseTerminalLoadDivisibility
 import DkMath.FLT.Seven.SevenBaseTerminalEndpointSeparation
+import DkMath.FLT.Seven.SevenBaseTerminalCarrierRouting
 import DkMath.FLT.Seven.SevenBaseTerminalAudit
 
 #print "file: DkMath.FLT.Seven"
