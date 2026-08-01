68b19527bcf35cc21e465f195befa5f32ef595b1
D. <Deskuma@users.noreply.github.com>
2026-08-02 02:25:43 +0900

    feat(RH): export CFBRC off-critical exclusion module

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -14,6 +14,7 @@ import DkMath.RH.EulerZeta
 import DkMath.RH.EulerZetaLemmas
 import DkMath.RH.HopcInfiniteLift
 import DkMath.RH.CFBRCBridge
+import DkMath.RH.CFBRC.OffCriticalExclusion
 import DkMath.RH.EulerZetaConvergence
 
 #print "file: DkMath.RH"
