da30e256d39b6a3de5c74770a2f2f0b558d85a60
D. <Deskuma@users.noreply.github.com>
2026-08-02 06:21:58 +0900

    feat(RH): export mirror root-of-unity classification

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -17,6 +17,7 @@ import DkMath.RH.CFBRCBridge
 import DkMath.RH.CFBRC.OffCriticalExclusion
 import DkMath.RH.CFBRC.OffCriticalExclusionGeneral
 import DkMath.RH.CFBRC.MirrorThreatModel
+import DkMath.RH.CFBRC.MirrorRootOfUnity
 import DkMath.RH.EulerZetaConvergence
 
 #print "file: DkMath.RH"
