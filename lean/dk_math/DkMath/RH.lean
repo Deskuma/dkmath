788a9068b16004e8da9a2568a3c3cd4e92bf8264
D. <Deskuma@users.noreply.github.com>
2026-08-02 07:44:58 +0900

    feat(RH): export finite closure permutation control

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -21,6 +21,7 @@ import DkMath.RH.CFBRC.MirrorRootOfUnity
 import DkMath.RH.CFBRC.MirrorAngleBranch
 import DkMath.RH.CFBRC.MirrorIndexedRoot
 import DkMath.RH.CFBRC.FiniteClosure
+import DkMath.RH.CFBRC.FiniteClosurePermutation
 import DkMath.RH.EulerZetaConvergence
 
 #print "file: DkMath.RH"
