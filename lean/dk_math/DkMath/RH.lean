b439176f8814ef4ccc5229f1f4aed0dbd6bc625c
D. <Deskuma@users.noreply.github.com>
2026-08-03 00:55:49 +0900

    chore(RH): export eta energy limit bridge

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -47,6 +47,7 @@ import DkMath.RH.Weave.Analytic.EtaPairedHolomorphic
 import DkMath.RH.Weave.Analytic.EtaPoleAudit
 import DkMath.RH.Weave.Analytic.EtaContinuationDomains
 import DkMath.RH.Weave.Analytic.EtaPairedContinuation
+import DkMath.RH.Weave.Analytic.EtaEnergyLimit
 import DkMath.RH.EulerZetaConvergence
 
 #print "file: DkMath.RH"
