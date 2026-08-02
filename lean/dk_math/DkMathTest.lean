857833cd0a2dfea9e03696e7563fa1cc410c6207
D. <Deskuma@users.noreply.github.com>
2026-08-03 05:05:27 +0900

    chore(test): export eta KUS mirror unit bridge tests

--- lean/dk_math/DkMathTest.lean
+++ lean/dk_math/DkMathTest.lean
@@ -10,5 +10,6 @@ import DkMathTest.ABC
 import DkMathTest.CFBRC
 import DkMathTest.KUS
 import DkMathTest.RH.CFBRCEtaMirrorUnitSplit
+import DkMathTest.RH.CFBRCEtaKUSMirrorUnitBridge
 
 #print "file: DkMathTest"
