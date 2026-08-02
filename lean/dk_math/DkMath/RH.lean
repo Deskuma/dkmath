2ae55f6a1c8ba03776f5ac7a2efe3fac71b2cb95
D. <Deskuma@users.noreply.github.com>
2026-08-03 04:50:37 +0900

    chore(RH): export eta mirror unit split

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -35,6 +35,7 @@ import DkMath.RH.CFBRC.EtaKUSState
 import DkMath.RH.CFBRC.EtaKUSLimit
 import DkMath.RH.CFBRC.EtaKUSProjectedCenterDecoder
 import DkMath.RH.CFBRC.EtaMirrorAmplitudeDecoder
+import DkMath.RH.CFBRC.EtaMirrorUnitSplit
 import DkMath.RH.CFBRC.EtaKUSMirrorAmplitudeBridge
 import DkMath.RH.CFBRC.EtaKUSDecoderAgreementAudit
 import DkMath.RH.CFBRC.ZeroLocusFactorBridge
