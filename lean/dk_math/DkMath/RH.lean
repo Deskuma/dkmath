1be7151de7391f02ac876670287d7bb80aba517d
D. <Deskuma@users.noreply.github.com>
2026-08-03 19:51:57 +0900

    feat: export eta paired rotating-frame geometry

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -59,6 +59,7 @@ import DkMath.RH.CFBRC.EtaCriticalMirrorPairedDefectDecay
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedDefectIntegral
 import DkMath.RH.CFBRC.EtaCriticalMirrorDefectKernelFactorization
 import DkMath.RH.CFBRC.EtaCriticalMirrorContinuousWeightPressure
+import DkMath.RH.CFBRC.EtaCriticalMirrorPairedRotatingFrame
 import DkMath.RH.CFBRC.EtaEndpointIncrementDecoder
 import DkMath.RH.Weave.Control.IndexShiftAudit
 import DkMath.RH.Weave.Finite.PairEnergy
