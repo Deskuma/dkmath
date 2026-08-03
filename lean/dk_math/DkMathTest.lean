ceb7424714094f0c6bf22a1a498991ae93c889d9
D. <Deskuma@users.noreply.github.com>
2026-08-03 17:55:13 +0900

    test(RH): include continuous mirror defect factorization regression

--- lean/dk_math/DkMathTest.lean
+++ lean/dk_math/DkMathTest.lean
@@ -26,6 +26,7 @@ import DkMathTest.RH.CFBRCEtaCriticalMirrorPhaseProjection
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedPhaseProjection
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedDefectDecay
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedDefectIntegral
+import DkMathTest.RH.CFBRCEtaCriticalMirrorDefectKernelFactorization
 import DkMathTest.RH.CFBRCEtaEndpointIncrementDecoder
 import DkMathTest.RH.WeaveEtaPairIntegral
 import DkMathTest.RH.WeaveEtaPairPhaseSpan
