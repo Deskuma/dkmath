a520727958507ee1fdd6e406da29d13cbe480edb
D. <Deskuma@users.noreply.github.com>
2026-08-03 20:36:45 +0900

    test: export paired defect tail regression

--- lean/dk_math/DkMathTest.lean
+++ lean/dk_math/DkMathTest.lean
@@ -30,6 +30,7 @@ import DkMathTest.RH.CFBRCEtaCriticalMirrorDefectKernelFactorization
 import DkMathTest.RH.CFBRCEtaCriticalMirrorContinuousWeightPressure
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedRotatingFrame
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedAbelTransform
+import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedTail
 import DkMathTest.RH.CFBRCEtaEndpointIncrementDecoder
 import DkMathTest.RH.WeaveEtaPairIntegral
 import DkMathTest.RH.WeaveEtaPairPhaseSpan
