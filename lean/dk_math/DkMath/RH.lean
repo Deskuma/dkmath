b53e5065f355c818387a4aac3dd68d2ce7169398
D. <Deskuma@users.noreply.github.com>
2026-08-04 02:01:01 +0900

    chore: export defect pair term eventual sign

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -64,6 +64,7 @@ import DkMath.RH.CFBRC.EtaCriticalMirrorDefectCoefficientMargin
 import DkMath.RH.CFBRC.EtaCriticalMirrorContinuousWeightThreshold
 import DkMath.RH.CFBRC.EtaCriticalMirrorDefectCoefficientEventualSign
 import DkMath.RH.CFBRC.EtaCriticalMirrorDefectKernelEventualSign
+import DkMath.RH.CFBRC.EtaCriticalMirrorDefectPairTermEventualSign
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedRotatingFrame
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedAbelTransform
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedTail
