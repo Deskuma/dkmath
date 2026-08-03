47428f6107073319c94e128398767ae6afa1e84c
D. <Deskuma@users.noreply.github.com>
2026-08-04 08:10:58 +0900

    chore(RH): export finite pair-frame block chord regression

--- lean/dk_math/DkMathTest.lean
+++ lean/dk_math/DkMathTest.lean
@@ -47,6 +47,7 @@ import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedAbelProjectionTail
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedFrameVariation
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedFrameGaugeAudit
 import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedFrameBlockAlignment
+import DkMathTest.RH.CFBRCEtaCriticalMirrorPairedFrameBlockChord
 import DkMathTest.RH.CFBRCEtaEndpointIncrementDecoder
 import DkMathTest.RH.WeaveEtaPairIntegral
 import DkMathTest.RH.WeaveEtaPairPhaseSpan
