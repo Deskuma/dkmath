11693c463654b8e21e8966116229f9f949040929
D. <Deskuma@users.noreply.github.com>
2026-08-04 08:22:33 +0900

    chore(RH): export quantitative defect-kernel margins

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -80,6 +80,7 @@ import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameGaugeAudit
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameBlockAlignment
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameBlockChord
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameBlockProjection
+import DkMath.RH.CFBRC.EtaCriticalMirrorDefectKernelQuantitativeMargin
 import DkMath.RH.CFBRC.EtaEndpointIncrementDecoder
 import DkMath.RH.Weave.Control.IndexShiftAudit
 import DkMath.RH.Weave.Finite.PairEnergy
