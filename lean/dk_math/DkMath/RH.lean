b43ab8ce86d463bd1f665fb7f60e7c613bfe7b07
D. <Deskuma@users.noreply.github.com>
2026-08-03 17:33:21 +0900

    feat(RH): export eta pair integral representation

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -65,6 +65,7 @@ import DkMath.RH.Weave.Analytic.EtaEvenPairing
 import DkMath.RH.Weave.Analytic.EtaPairedLimit
 import DkMath.RH.Weave.Analytic.EtaTermDecay
 import DkMath.RH.Weave.Analytic.EtaPairDerivative
+import DkMath.RH.Weave.Analytic.EtaPairIntegral
 import DkMath.RH.Weave.Analytic.EtaAbsoluteConvergence
 import DkMath.RH.Weave.Analytic.EtaFiniteFactorization
 import DkMath.RH.Weave.Analytic.EtaZetaIdentification
