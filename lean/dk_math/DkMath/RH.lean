5dd8af10a271650b7311a5c318260b823d6b9852
D. <Deskuma@users.noreply.github.com>
2026-08-04 15:50:28 +0900

    chore(RH): export positive-density paired blocks

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -92,6 +92,7 @@ import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameGrowingBlockTailRemainder
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameAbelTailIdentity
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameAbelCorrectionTailBound
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameMovingProjectionTailMargin
+import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFramePositiveDensityBlock
 import DkMath.RH.CFBRC.EtaEndpointIncrementDecoder
 import DkMath.RH.Weave.Control.IndexShiftAudit
 import DkMath.RH.Weave.Finite.PairEnergy
