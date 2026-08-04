6a2b7625ddda26d8c272cff063c1e510a66d1523
D. <Deskuma@users.noreply.github.com>
2026-08-04 16:08:26 +0900

    chore(RH): export defect margin power lower bounds

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -93,6 +93,7 @@ import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameAbelTailIdentity
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameAbelCorrectionTailBound
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFrameMovingProjectionTailMargin
 import DkMath.RH.CFBRC.EtaCriticalMirrorPairedFramePositiveDensityBlock
+import DkMath.RH.CFBRC.EtaCriticalMirrorDefectPairMarginPowerLowerBound
 import DkMath.RH.CFBRC.EtaEndpointIncrementDecoder
 import DkMath.RH.Weave.Control.IndexShiftAudit
 import DkMath.RH.Weave.Finite.PairEnergy
