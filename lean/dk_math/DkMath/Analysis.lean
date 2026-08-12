1879e2a9483a001c4f6f304644b35efd358d486c
Deskuma <deskuma.software@gmail.com>
2026-08-12 11:35:31 +0900

    Add: XDP-006: Mellin centered dilation / second-difference

--- lean/dk_math/DkMath/Analysis.lean
+++ lean/dk_math/DkMath/Analysis.lean
@@ -15,6 +15,7 @@ import DkMath.Analysis.TaylorBridge
 import DkMath.Analysis.MellinCriticalMirror
 import DkMath.Analysis.MellinCompactSupport
 import DkMath.Analysis.MellinCompactSupportHolomorphic
+import DkMath.Analysis.MellinCenteredDilation
 import DkMath.Analysis.DkReal
 import DkMath.Analysis.DkReal.SemanticCF2D
 import DkMath.Analysis.DkReal.SemanticCF2DPhase
--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -39,6 +39,7 @@ import DkMath.RH.CFBRC.PascalCenteredXiMultiplicityLocalChargeBridge
 import DkMath.RH.CFBRC.PascalCenteredXiGlobalZeroDiskBridge
 import DkMath.RH.CFBRC.PascalCenteredXiSafeRadiusAnnulusBridge
 import DkMath.RH.CFBRC.PascalCenteredXiMellinWeightedOuterContourBridge
+import DkMath.RH.CFBRC.PascalCenteredXiMellinSecondDifferenceBridge
 import DkMath.RH.CFBRC.PascalCenteredXiOuterContourResidueBridge
 import DkMath.RH.CFBRC.PascalCenteredXiRadialLayerCakeOuterCountBridge
 import DkMath.RH.CFBRC.PascalCenteredXiFixedSecondMomentDefectBridge
