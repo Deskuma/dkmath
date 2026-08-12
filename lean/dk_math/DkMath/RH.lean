ab70d61ff9f6a7c58945b4dc5f66361dd74e2637
Deskuma <deskuma.software@gmail.com>
2026-08-12 12:52:57 +0900

    Add: XDP-008: Completed-zeta logarithmic-derivative decomposition / explicit-formula transport preflight

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -41,6 +41,7 @@ import DkMath.RH.CFBRC.PascalCenteredXiSafeRadiusAnnulusBridge
 import DkMath.RH.CFBRC.PascalCenteredXiMellinWeightedOuterContourBridge
 import DkMath.RH.CFBRC.PascalCenteredXiMellinSecondDifferenceBridge
 import DkMath.RH.CFBRC.PascalCenteredXiMellinQuadraticRealizationBridge
+import DkMath.RH.CFBRC.PascalCenteredXiCompletedZetaLogDerivBridge
 import DkMath.RH.CFBRC.PascalCenteredXiOuterContourResidueBridge
 import DkMath.RH.CFBRC.PascalCenteredXiRadialLayerCakeOuterCountBridge
 import DkMath.RH.CFBRC.PascalCenteredXiFixedSecondMomentDefectBridge
