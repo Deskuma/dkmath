f97d8f03ae13a1ec1bb819f3fe5915d5f68a5fd0
Deskuma <deskuma.software@gmail.com>
2026-08-16 06:45:59 +0900

    Add: CFZP-0020: CFZP-006P zero-cutoff contact baseline public decomposition audit

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -47,6 +47,7 @@ import DkMath.RH.CFBRC.CosmicFormulaZetaForwardIntegratedPolarizedMassAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaIntegratedPolarizedBalanceThresholdAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaContactThresholdDecompositionAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaContactThresholdPolarizationBridgeAudit
+import DkMath.RH.CFBRC.CosmicFormulaZetaZeroCutoffContactBaselineAudit
 import DkMath.RH.CFBRC.PascalVonMangoldtLSeriesBridge
 import DkMath.RH.CFBRC.PascalZetaLogDerivativeZeroBridge
 import DkMath.RH.CFBRC.PascalCriticalMirrorZeroWindowEnergyBridge
