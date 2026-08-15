8a4883447badaddb0ef2bcda1f94ffadddcda4c9
Deskuma <deskuma.software@gmail.com>
2026-08-16 03:37:24 +0900

    Add: CFZP-0012: CFZP-006H top-edge Gram specialization / dual-quadraticization boundary

--- lean/dk_math/DkMath/RH.lean
+++ lean/dk_math/DkMath/RH.lean
@@ -39,6 +39,7 @@ import DkMath.RH.CFBRC.CosmicFormulaZetaOffDiagonalPairGramAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaSignedSpectralNodeFactorizationAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaFullSignedMellinGramBridgeAudit
 import DkMath.RH.CFBRC.CosmicFormulaZetaFullSignedGramLimitRecoveryAudit
+import DkMath.RH.CFBRC.CosmicFormulaZetaTopEdgeGramSpecializationAudit
 import DkMath.RH.CFBRC.PascalVonMangoldtLSeriesBridge
 import DkMath.RH.CFBRC.PascalZetaLogDerivativeZeroBridge
 import DkMath.RH.CFBRC.PascalCriticalMirrorZeroWindowEnergyBridge
