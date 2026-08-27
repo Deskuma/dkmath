69dccbc75d4c7424e849b9a2437d7bc504cc4e85
Deskuma <deskuma.software@gmail.com>
2026-08-27 12:23:01 +0900

    impl: PRIM-L067 Fifth-Direction Packet / Fifth-Power Gate / Extra Support Charge

--- lean/dk_math/DkMath/NumberTheory/Legendre.lean
+++ lean/dk_math/DkMath/NumberTheory/Legendre.lean
@@ -48,6 +48,7 @@ import DkMath.NumberTheory.Legendre.ParitySafeNearFirstPrimeWaveCapacity
 import DkMath.NumberTheory.Legendre.ParitySafeFourthDualBaseCapacity
 import DkMath.NumberTheory.Legendre.ParitySafeFullCoverCapacityFrontier
 import DkMath.NumberTheory.Legendre.ParitySafeDepthResidualFifthTrigger
+import DkMath.NumberTheory.Legendre.ParitySafeFifthDirectionGate
 
 #print "file: DkMath.NumberTheory.Legendre"
 
